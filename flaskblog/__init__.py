f8bc054e704d0c9b274fe9451b04cbe0e56dcb22
kittypolly <75717663+kittypolly@users.noreply.github.com>
2021-08-03 13:12:01 +0900

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,15 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-def create_app(config_file='settings.py'):
-    app = Flask(__name__)
-    #시크릿 키를 만들어서 외부로 부터 보호 합니다.
-    app.config['SECRET_KEY'] = 'eb64a3d88e9c8cb71adef6dd47270844'
-    app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
-    db = SQLAlchemy(app)
-    Bcrypt = Bcrypt(app)
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'login'
-    login_manager.login_message_category = 'info'
+app = Flask(__name__)
+#시크릿 키를 만들어서 외부로 부터 보호 합니다.
+app.config['SECRET_KEY'] = 'eb64a3d88e9c8cb71adef6dd47270844'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+db = SQLAlchemy(app)
+Bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
-    from flaskblog import routes
+from flaskblog import routes
